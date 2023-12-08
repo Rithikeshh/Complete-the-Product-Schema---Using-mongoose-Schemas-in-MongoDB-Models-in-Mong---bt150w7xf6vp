b111a5d0ce4e6f2ef4098d7bf3c60f1459745d47
Rithikeshh <rithikeshh@gmail.com>
2023-12-08 10:13:25 +0530

    updated commit

--- models/product.js
+++ models/product.js
@@ -1,6 +1,22 @@
 
+const { boolean } = require("joi");
 var mongoose= require("mongoose");
 
 //write your schema Here with name of schema as productSchema
+const productSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    Quantity:{
+        type:Number,
+        required: true
+    },
+    Price:{
+        type:Number,
+        required: true
+    },
+    instock: String
+})
 
 module.exports =mongoose.model("products",productSchema);
